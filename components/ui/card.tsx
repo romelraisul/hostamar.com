741b3162cad4c3e779aee67e68f904b665e2ca51
Romel <romelraisul@gmail.com>
2026-06-27 00:36:01 +0600

    fix: add missing ui components stub for build

---
+++ components/ui/card.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+export function Card(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props} />
+}
+
+export function CardHeader(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props} />
+}
+
+export function CardContent(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props} />
+}
+
+export function CardTitle(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props} />
+}
