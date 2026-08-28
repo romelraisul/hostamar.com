77b25e242f7828ade94267c7f354203b535a24ba
Romel Raisul <romelraisul@gmail.com>
2026-08-28 08:17:09 +0600

    fix: whitelist /api/tv/stable-channels in middleware publicApiPaths

--- middleware.ts
+++ middleware.ts
@@ -111,6 +111,7 @@ export async function middleware(request: NextRequest) {
     '/api/tv/hls-url',
     '/api/tv/playlist',
     '/api/tv/channels',
+    '/api/tv/stable-channels',
     '/api/tv/iptv.m3u',
     '/api/tv/epg.xml',
     '/api/tv/now-playing',
